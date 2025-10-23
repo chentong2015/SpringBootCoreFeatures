3f415146c65c5a0a54d0d7125ee01ed02394d835
chent5 <chen2015tong@gmail.com>
2025-10-23 16:56:09 +0200

    update thymeleaf web engine

--- spring-boot-web-thymeleaf/src/main/java/thymeleaf/controller/BaseController.java
+++ spring-boot-web-thymeleaf/src/main/java/thymeleaf/controller/BaseController.java
@@ -3,15 +3,21 @@ package thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BaseController {
 
+    // TODO. 重定向到static静态资源页面
     @GetMapping("/")
     public String index(Model model) {
-        return "redirect:/home.html";
+        return "redirect:/static.html";
+    }
+
+    // TODO. 重定向到templates模板页面
+    @GetMapping(value = "setup")
+    public String setup() {
+        System.out.println("Process the input for playing ...");
+        return "redirect:/home";
     }
 
     @GetMapping("home")
@@ -20,11 +26,5 @@ public class BaseController {
         model.addAttribute("value", "About page");
         model.addAttribute("resultMessage", "This is the result message");
         return "home";
-    }
-
-    @PostMapping(value = "play")
-    public String play(@RequestParam int input) {
-        System.out.println("Process the input for playing ...");
-        return "redirect:/home";
     }
 }
